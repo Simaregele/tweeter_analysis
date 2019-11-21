8009ce93748d4abd2872fab4034b09b5506eb156
simaregele <dtkjcbgtlbcn1306>
2019-11-22 00:32:26 +0500

    Жду когда твитер даст доступ к апи

---
+++ main.py
@@ -0,0 +1,4 @@
+# https://github.com/vprusso/youtube_tutorials/tree/master/twitter_python
+# https://www.youtube.com/watch?v=1gQ6uG5Ujiw -  видео тутриал так-же ссылки есть в гитхабе
+
+import config
